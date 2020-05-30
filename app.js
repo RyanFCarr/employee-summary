334581a8fa6cec7facc252fe8760d94174f4d72c
Ryan Carr <ryanfcarr@gmail.com>
2020-05-29 22:57:19 -0400

    Added additional Engineer, intern prompts and HTML redering

--- app.js
+++ app.js
@@ -4,42 +4,69 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const questions = require("./utils/questions.json");
+const questions = require("./utils/questions");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+let finished = false;
 
 // Prompt Manager
-const prompts = questions.employee;
-prompts.push(...questions.manager);
-inquirer.prompt(prompts)
-.then(answers => {
-    const { name, id, email, officeNumber } = answers
-    const mgr = new Manager(name, id, email, officeNumber);
-});
+async function promptManager(){
+    const prompts = [...questions.employee];
+    prompts.push(...questions.manager);
+
+    const answers = await inquirer.prompt(prompts);
+
+    const { name, id, email, officeNumber } = answers;
+    return new Manager(name, id, email, officeNumber);
+}
+
+// Prompt Engineer
+async function promptEngineer(){
+    const prompts = [...questions.employee];
+    prompts.push(...questions.engineer);
+
+    const answers = await inquirer.prompt(prompts);
+
+    const { name, id, email, gitHub, done } = answers
+    finished = !done;
+    return new Engineer(name, id, email, gitHub); 
+} 
+
+// Prompt Intern
+async function promptIntern(){
+    const prompts = [...questions.employee];
+    prompts.push(...questions.intern);
+
+    const answers = await inquirer.prompt(prompts);
+
+    const { name, id, email, schoolName, done } = answers
+    finished = !done;
+    return new Intern(name, id, email, schoolName);
+}
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+async function getEmployees(){
+    const employees = [];
+    employees.push(await promptManager());
+ 
+    finished = false;
+    while(finished === false){
+        employees.push(await promptEngineer());
+    }
+   
+    finished = false;
+    while(finished === false){
+        employees.push(await promptIntern());
+    }
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    return employees;
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+getEmployees().then(employees => {
+    fs.mkdirSync(OUTPUT_DIR, { recursive:true })
+fs.writeFile(outputPath, render(employees), () => console.log('HTML Generated'));
+})
